d5adb3967963f9c7ce6f892a468273edc7ca8e37
Felix <felix.j.jung@gmail.com>
2021-10-07 03:32:24 +0200

    Initial Commit

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour
+{
+    public Button startButton;
+    public Button messageButton;
+    public Label messageText; 
+    
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+        startButton = root.Q<Button>("start-button");
+        messageButton = root.Q<Button>("message-button");
+        messageText = root.Q<Label>("message-text");
+
+        startButton.clicked += StartButtonPressed;
+        messageButton.clicked += MessageButtonPressed;
+        
+
+
+    }
+
+    void StartButtonPressed()
+    {
+    
+        SceneManager.LoadScene("game");
+
+    }
+    void MessageButtonPressed()
+    {
+        messageText.text = "Hello World";
+        messageText.style.display = DisplayStyle.Flex;
+
+    }
+
+  
+}
