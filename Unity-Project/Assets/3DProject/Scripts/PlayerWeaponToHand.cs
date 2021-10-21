73c40c2869b2182dc8b5a4cad187b25f3223cbca
Felix <felix.j.jung@gmail.com>
2021-10-21 18:20:45 +0200

    Reboot

---
+++ Unity-Project/Assets/3DProject/Scripts/PlayerWeaponToHand.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace _3DProject.Scripts
+{
+    public class PlayerWeaponToHand : MonoBehaviour
+    {
+        [SerializeField] private GameObject leftWeaponOrigin;
+
+        [SerializeField] private GameObject itemIndex;
+
+
+        private void Awake()
+        {
+            SetTransform();
+        }
+
+        private void SetTransform()
+        {
+            itemIndex.transform.position = leftWeaponOrigin.transform.position;
+            itemIndex.transform.parent = leftWeaponOrigin.transform;
+        }
+    }
+}
---
+++ Unity-Project/Assets/3DProject/Scripts/ThirdPersonShootingController.cs
@@ -0,0 +1,114 @@
+using Cinemachine;
+using StarterAssets;
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+using UnityEngine.VFX;
+
+namespace _3DProject.Scripts
+{
+    public class ThirdPersonShootingController : MonoBehaviour
+    {
+        [SerializeField] private Transform aimTarget;
+        [SerializeField] private GameObject armAim;
+
+
+        [SerializeField] private Camera fpsCam;
+        [SerializeField] CinemachineVirtualCamera aimVirtualCamera;
+        [SerializeField] private Transform weaponout;
+        [SerializeField] private VisualEffect visualEffect;
+        private readonly float damage = 10f;
+
+        private readonly float range = 100f;
+
+
+        private Rig rig;
+
+        private StarterAssetsInputs starterAssetsInputs;
+
+        private void Awake()
+
+        {
+            rig = armAim.GetComponentInParent<Rig>();
+
+            fpsCam = Camera.main;
+            starterAssetsInputs = GetComponent<StarterAssetsInputs>();
+        }
+
+        private void Update()
+        {
+            //TODO Replace with New Input System
+
+            RaycastHit hit;
+
+
+            #region Aim + Shoot
+
+            //calculate ray from camera towards crosshair
+            if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range) &&
+                Input.GetButtonDown("Fire1"))
+            {
+                Shoot();
+            }
+
+
+            if (starterAssetsInputs.aim)
+            {
+                if (hit.point == Vector3.zero)
+                {
+                    rig.weight = 0;
+                }
+                else
+                {
+                    rig.weight = 1;
+                }
+
+                aimTarget.position = hit.point;
+
+                Vector3 hitPoint = hit.point;
+                var transform1 = transform;
+                var position = transform1.position;
+                hitPoint.y = position.y;
+                Vector3 aimDirection = (hitPoint - position).normalized;
+
+                transform.forward = Vector3.Lerp(transform1.forward, aimDirection, Time.deltaTime * 20f);
+
+
+                aimVirtualCamera.gameObject.SetActive(true);
+            }
+            else
+            {
+                rig.weight = 0;
+                aimVirtualCamera.gameObject.SetActive(false);
+            }
+
+            #endregion
+        }
+
+
+        void Shoot()
+        {
+            RaycastHit hit;
+
+            visualEffect.Play();
+
+
+            #region Checker
+
+            if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+            {
+                Target target = hit.transform.GetComponent<Target>();
+                if (target != null)
+                {
+                    target.TakeDamage(damage);
+                }
+
+                Debug.DrawLine(weaponout.position, hit.point, Color.red, 2);
+
+
+                Debug.Log(hit.transform.name);
+            }
+
+            #endregion
+        }
+    }
+}
